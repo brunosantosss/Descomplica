05bb5b334eb6420421dc5be3627f1b2676fce225
brunosantosss <brunosantosoliveira778@gmail.com>
2023-03-23 20:27:40 -0300

    add suporte para dispositivos moveis

--- public/src/main.js
+++ public/src/main.js
@@ -1,19 +1,65 @@
-const button = document.querySelector('button');
-const text = document.querySelector('.text');
+/*
+    Definition of variables.
+    question_area = get the value of the textarea ("Qual sua dúvida?").
+    chat_area = conversation field between machine and user.
+    btn_mic = get microphone button
+    Recognition = initialize the initRecognition class for voice recognition
+*/
+const question_area = document.getElementById('question_area');
+const chat_area = document.getElementById('chat-area');
+const btn_mic = document.getElementById('btn_mic');
+const Recognition = initRecognition(); 
+var Listening = false; 
 
-const Recognition = initRecognition(); // inicializo o recognition.
-var Listening = false; // flag controladora. que diz se o sistema está ou não escutando.
+question_area.addEventListener("keypress", (e) => {
+    var initial_container = document.getElementById('initial-container');
+    var chat_area = document.getElementById('chat-area');
 
-button.addEventListener('click', (e) => {
+    if(question_area.value && e.key === "Enter") {
+        initial_container.classList.add("hide");
+
+        setTimeout(() => {
+            initial_container.classList.add("hidden");
+            chat_area.classList.remove("hidden");
+            chat_area.classList.add("block");
+        }, 600);
+        
+        SendQuestion(question_area.value);
+    }
+});
+
+btn_mic.addEventListener('click', (e) => {
     if(!Recognition) return;
+    
+    var mic_text = document.getElementById("mic-text");
 
-    button.innerHTML = Listening ? 'Pressione para falar' : 'Pressione para parar';
-    
-    button.classList.toggle("bg-indigo-500");
-    button.classList.toggle("bg-red-600");
-    
-    Listening ? Recognition.stop() : Recognition.start();
-})
+    if(Listening) {
+        var initial_container = document.getElementById('initial-container');
+        var chat_area = document.getElementById('chat-area');
+        mic_text.innerHTML = 'Clique no Microfone falar';
+        Recognition.stop();
+        
+        initial_container.classList.add("hide");
+
+        setTimeout(() => {
+            initial_container.classList.add("hidden");
+            chat_area.classList.remove("hidden");
+            chat_area.classList.add("block");
+        }, 600);
+    } 
+    else if(Recognition) {
+        mic_text.innerHTML = 'Clique no Microfone parar de falar';
+        Recognition.start();
+    }
+    else {
+        mic_text.innerHTML = 'Clique no Microfone parar de falar';
+        Recognition.start();
+    }
+});
+
+/*
+    Creation of Functions
+*/
 
 function initRecognition() {  
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -21,7 +67,7 @@ function initRecognition() {
     const gRecognition = SpeechRecognition !== undefined ? new SpeechRecognition() : null;
 
     if(!gRecognition) {
-        text.innerHTML("Infelizmente seu navegador não possui compatibilidade com a API SpeechRecogninition :(");
+        alert("Infelizmente seu navegador atual não possui suporte ao uso de Microfone. Tente utilizar o campo de digitação")
         return null;
     }
 
@@ -29,7 +75,56 @@ function initRecognition() {
     gRecognition.onstart = () => Listening = true;
     gRecognition.onend = () => Listening = false;
     gRecognition.onerror = (e) => console.log(`SpeechRecognition ERROR: ${e}`);
-    gRecognition.onresult = (e) => text.innerHTML = e.results[0][0].transcript;
-
+    gRecognition.onresult = (e) => {
+        SendQuestion(e.results[0][0].transcript);        
+    };
+    
     return gRecognition;
+}
+
+function SendQuestion(question) {
+    
+    if(!question)
+        return chat_area.value += `Descomplica:   Desculpe, não entendi :(\n\n`
+
+    fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + "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",
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: question,
+            max_tokens: 2048, // tamanho da resposta
+            temperature: 1, // criatividade na resposta
+        }),
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      if (chat_area.value) chat_area.value += "\n";
+
+      if (json.error?.message) chat_area.value += `Infelizmente aconteceu algum erro.`;
+
+      else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Não consegui achar uma respota para sua dúvida :(";
+        chat_area.value += `Descomplica:   ${text}`;
+      }
+
+      chat_area.scrollTop = chat_area.scrollHeight;
+    })
+    .finally(() => {
+      question_area.value = "";
+      question_area.disabled = false;
+      question_area.focus();
+    });
+
+  if (chat_area.value) chat_area.value += "\n\n";
+
+  chat_area.value += `Você:   ${question}` + '\n\n';
+  question_area.value = "Gerando resposta..";
+  question_area.disabled = true;
+
+  chat_area.scrollTop = chat_area.scrollHeight;
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-const OPENAI_API_KEY = "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";
+/*
+    Definition of variables.
+    question_area = get the value of the textarea ("Qual sua dúvida?").
+    chat_area = conversation field between machine and user.
+    btn_mic = get microphone button
+    Recognition = initialize the initRecognition class for voice recognition
+*/
 const question_area = document.getElementById('question_area');
 const chat_area = document.getElementById('chat-area');
+const btn_mic = document.getElementById('btn_mic');
+const Recognition = initRecognition(); 
+var Listening = false; 
 
-// Usando o campo de digitação
 question_area.addEventListener("keypress", (e) => {
     var initial_container = document.getElementById('initial-container');
     var chat_area = document.getElementById('chat-area');
@@ -16,17 +24,9 @@ question_area.addEventListener("keypress", (e) => {
             chat_area.classList.add("block");
         }, 600);
         
-        sQuestion = question_area.value;
-        SendQuestion();
+        SendQuestion(question_area.value);
     }
 });
-
-// Usando o microfone
-const btn_mic = document.getElementById('btn_mic');
-const Recognition = initRecognition(); // inicializo o recognition.
-var Listening = false; // flag controladora. que diz se o sistema está ou não escutando.
-
-var sQuestion;
 
 btn_mic.addEventListener('click', (e) => {
     if(!Recognition) return;
@@ -46,7 +46,6 @@ btn_mic.addEventListener('click', (e) => {
             chat_area.classList.remove("hidden");
             chat_area.classList.add("block");
         }, 600);
-    
     } 
     else if(Recognition) {
         mic_text.innerHTML = 'Clique no Microfone parar de falar';
@@ -58,13 +57,17 @@ btn_mic.addEventListener('click', (e) => {
     }
 });
 
+/*
+    Creation of Functions
+*/
+
 function initRecognition() {  
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     
     const gRecognition = SpeechRecognition !== undefined ? new SpeechRecognition() : null;
 
     if(!gRecognition) {
-        text.innerHTML("Infelizmente seu navegador não possui compatibilidade com a API SpeechRecogninition :(");
+        alert("Infelizmente seu navegador atual não possui suporte ao uso de Microfone. Tente utilizar o campo de digitação")
         return null;
     }
 
@@ -73,28 +76,27 @@ function initRecognition() {
     gRecognition.onend = () => Listening = false;
     gRecognition.onerror = (e) => console.log(`SpeechRecognition ERROR: ${e}`);
     gRecognition.onresult = (e) => {
-        sQuestion = e.results[0][0].transcript;
-
-        if(sQuestion)
-            return SendQuestion();
-            
-        chat_area.value += `Descomplica:   Desculpe, não entendi :(\n\n`
+        SendQuestion(e.results[0][0].transcript);        
     };
-
+    
     return gRecognition;
 }
 
-function SendQuestion() {
+function SendQuestion(question) {
+    
+    if(!question)
+        return chat_area.value += `Descomplica:   Desculpe, não entendi :(\n\n`
+
     fetch("https://api.openai.com/v1/completions", {
         method: "POST",
         headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + OPENAI_API_KEY,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + "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",
         },
         body: JSON.stringify({
             model: "text-davinci-003",
-            prompt: sQuestion,
+            prompt: question,
             max_tokens: 2048, // tamanho da resposta
             temperature: 1, // criatividade na resposta
         }),
@@ -103,10 +105,10 @@ function SendQuestion() {
     .then((json) => {
       if (chat_area.value) chat_area.value += "\n";
 
-      if (json.error?.message) chat_area.value += `Infelizmente aconteceu algum erro :(\nError CODE: ${json.error.message}`;
+      if (json.error?.message) chat_area.value += `Infelizmente aconteceu algum erro.`;
 
       else if (json.choices?.[0].text) {
-        var text = json.choices[0].text || "";
+        var text = json.choices[0].text || "Não consegui achar uma respota para sua dúvida :(";
         chat_area.value += `Descomplica:   ${text}`;
       }
 
@@ -120,7 +122,7 @@ function SendQuestion() {
 
   if (chat_area.value) chat_area.value += "\n\n";
 
-  chat_area.value += `Você:   ${sQuestion}` + '\n\n';
+  chat_area.value += `Você:   ${question}` + '\n\n';
   question_area.value = "Gerando resposta..";
   question_area.disabled = true;
 
